a2a90f67647acd962e377c5dbffc2d4cffd108ea
Soumy Setu Choubey <105278271+soumysetu@users.noreply.github.com>
2022-10-13 21:37:15 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int myArray[4]={2,33,222,444};
+    for(int i=0;i<4;i++)
+    {
+        cout << i+1 <<" -> "<<myArray[i]<<endl;
+    }
+return 0;
+}
